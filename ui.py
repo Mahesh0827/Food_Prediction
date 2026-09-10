655ae860ba7c60cd9b722544fd00a31fcb4922e0
Mahesh0827 <velugotireddy0827@gmail.com>
2026-09-10 11:38:21 +0530

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+
+
+st.title("📄Score predictor ")
+study=st.slider("Study Time",0,10)
+atd=st.slider("Attended Days",0,80)
+gen=st.selectbox("Gender",["Male","Female"])
+
+gender=1 if(gen=="Male") else 0
+
+if(st.button("Predict the score")):
+    data ={
+        "study_time":study,
+        "attendance":atd,
+        "gender_Male":gender
+    }
+    res=requests.post("https://score-train-1.onrender.com//predict",json=data)
+    result=res.json()
+    st.write("The Predicted score is",result["Predicted_score"])
+    
